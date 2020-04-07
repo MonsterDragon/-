4ab7d84c85f0579a54936a2941bda3466592ab4a
root <1962610298@qq.com>
2020-04-07 14:31:28 +0800

    update 11

--- c/exe_1/11.c
+++ c/exe_1/11.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void Merge(int *L1, int *L2)
+// 此方法的时间复杂度为O(n)，空间复杂度也为O(n)
+
+void Merge_1(int *L1, int *L2, int Length)
 {
-	int Length = sizeof(L1)/sizeof(int);
 	printf("Length is %d\n", Length);
 	int *mergeList = (int *)malloc(Length * sizeof(int) * 2);
 	int i, j, k;
@@ -38,11 +39,40 @@ void Merge(int *L1, int *L2)
 	{
 		printf("mergeList[%d] : %d\n", i, mergeList[i]);
 	}
+	printf("mid num %d is %d\n", (Length*2-1)/2, mergeList[(Length*2-1)/2]);
+}
+
+// 此方法的时间复杂度是O(n/2) = O(n)，空间复杂度是O(1)
+Merge_2(int *L1, int *L2, int n)
+{
+	int i, j, k, num;
+	for (i = 0, j = 0, k = 0; k <= (n*2-1)/2; )
+	{
+		while (L1[i] < L2[j])
+		{
+			num = L1[i++];
+			k++;
+		}
+		while (L1[i] > L2[j])
+		{
+			num = L2[j++];
+			k++;
+		}
+		while (L1[i] == L2[j])
+		{
+			num = L1[i++];
+			j++;
+			k += 2;
+		}
+	}
+	printf("mid num %d is %d\n", (n*2-1)/2, num);
 }
 
 int main()
 {
 	int data1[] = {1, 2, 5, 8, 10};
 	int data2[] = {3, 4, 5, 9, 12};
-	Merge(data1, data2);
+	int length = sizeof(data1) / sizeof(int);
+	// Merge_1(data1, data2, length);
+	Merge_2(data1, data2, length);
 }
