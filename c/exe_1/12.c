66964f0626a89b2a9d2e55fa4d2f3cbb6f3187f6
root <1962610298@qq.com>
2020-04-08 18:18:56 +0800

    change 12

--- c/exe_1/12.c
+++ c/exe_1/12.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-// 时间复杂度是冒泡排序的O(n^2)，空间复杂度是O(n)
+// 时间复杂度是冒泡排序的O(n^2)，空间复杂度是O(1)。算法原地工作是指算法所需的辅助空间为常量
 int ResearchMain(int *data, int n)
 {
 	int *L = (int *)malloc((n - 1) * sizeof(int));
@@ -31,12 +31,40 @@ int ResearchMain(int *data, int n)
 	printf("the main num is %d\n", data[i - 1]);
 }
 
+// 时间复杂度是O(n)，空间复杂度是O(n)
+int SpacetoTime(int *L, int n)
+{
+	int *statics = (int *)malloc(n * sizeof(int));
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		statics[i] = 0;
+	}
+	for (i = 0; i < n; i++)
+	{
+		statics[L[i]]++;
+	}
+	for (i = 0; i < n; i++)
+	{
+		printf("statics[%d] : %d\n", i, statics[i]);
+	}
+	for (i = 0; i < n; i++)
+	{
+		if (statics[i] >= n / 2)
+		{
+			return i;
+		}
+	}
+}
+
 int main()
 {
 	int result, length;
 	int data1[] = {1, 3, 3, 3, 2, 6, 8, 10, 3, 6, 3, 3};
 	length = sizeof(data1)/sizeof(int);
-	result = ResearchMain(data1, length);
+	// result = ResearchMain(data1, length);
+	result = SpacetoTime(data1, length);
+	printf("result is %d\n", result);
 	int i;
 	for (i = 0; i < length; i++)
 	{
