f388481318f91a560e379e15980d0a57fddb81bf
root <1962610298@qq.com>
2020-07-05 16:27:50 +0800

    二叉树的三种遍历

---
+++ fish_c/chapter_5_树/01_二叉树/02_树的遍历/02_先序遍历.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct BiTNode
+{
+	int data;
+	struct BiTNode *lchild, *rchild;	
+}BiTNode, *BiTree;
+
+void preOrder(BiTNode *root)
+{
+	if (root == NULL)
+	{
+		return;
+	}
+
+	printf("%d\n", root -> data);
+	
+	preOrder(root -> lchild);
+
+	preOrder(root -> rchild);
+}
+
+int main()
+{
+		
+	BiTNode t1, t2, t3, t4, t5;
+
+	memset(&t1, 0, sizeof(BiTNode));
+	memset(&t2, 0, sizeof(BiTNode));
+	memset(&t3, 0, sizeof(BiTNode));
+	memset(&t4, 0, sizeof(BiTNode));
+	memset(&t5, 0, sizeof(BiTNode));
+
+	t1.data = 10;
+	t2.data = 20;
+	t3.data = 30;
+	t4.data = 40;
+	t5.data = 50;
+
+	t1.lchild = &t2;
+	t1.rchild = &t3;
+	t2.lchild = &t4;
+	t3.lchild = &t5;
+
+	preOrder(&t1);
+}
